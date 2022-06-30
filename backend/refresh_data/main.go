7363156763fbe65285875073e383c7da4e4f4a34
Tom Draper <tomjdraper1@gmail.com>
2022-06-30 16:10:05 +0100

    refactor

--- backend/refresh_data/main.go
+++ backend/refresh_data/main.go
@@ -1,67 +1,69 @@
 package main
 
 import (
+	"backend/database"
+	"fmt"
 	"net/http"
-	"refresh/lib/database"
+	"sync"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// func pingAddress(address string) {
-// 	pinger, err := ping.NewPinger(address)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	pinger.Count = 3
-// 	pinger.Timeout = time.Second * 3
-// 	pinger.SetPrivileged(true)
+func pingAddress(address string) {
+	pinger, err := ping.NewPinger(address)
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = 3
+	pinger.Timeout = time.Second * 3
+	pinger.SetPrivileged(true)
 
-// 	pinger.OnFinish = func(stats *ping.Statistics) {
-// 		fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
-// 		fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
-// 			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
-// 		fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
-// 			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
-// 		ping := database.Ping{Loss: stats.PacketLoss, Response: int64(stats.AvgRtt), Time: time.Now().UTC()}
-// 		database.UpdateDatabase(address, ping)
-// 	}
+	pinger.OnFinish = func(stats *ping.Statistics) {
+		fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+		fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+		fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+		ping := database.Ping{Loss: stats.PacketLoss, Response: int64(stats.AvgRtt), Time: time.Now().UTC()}
+		database.UpdateDatabase(address, ping)
+	}
 
-// 	err = pinger.Run()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+	err = pinger.Run()
+	if err != nil {
+		panic(err)
+	}
+}
 
-// func pingAsyncContinuous() {
-// 	for _, address := range database.Websites {
-// 		go pingAddress(address)
-// 	}
-// 	for range time.Tick(time.Minute * 60) {
-// 		for _, address := range database.Websites {
-// 			go pingAddress(address)
-// 		}
-// 	}
-// }
+func pingAsyncContinuous() {
+	for _, address := range database.Websites {
+		go pingAddress(address)
+	}
+	for range time.Tick(time.Minute * 60) {
+		for _, address := range database.Websites {
+			go pingAddress(address)
+		}
+	}
+}
 
-// func pingAsync() {
-// 	var wg sync.WaitGroup
-// 	for _, address := range database.Websites {
-// 		wg.Add(1)
-// 		go func(address string) {
-// 			defer wg.Done()
-// 			pingAddress(address)
-// 		}(address)
-// 	}
-// 	wg.Wait()
-// }
+func pingAsync() {
+	var wg sync.WaitGroup
+	for _, address := range database.Websites {
+		wg.Add(1)
+		go func(address string) {
+			defer wg.Done()
+			pingAddress(address)
+		}(address)
+	}
+	wg.Wait()
+}
 
-// func pingSync() {
-// 	for _, address := range database.Websites {
-// 		pingAddress(address)
-// 	}
-// }
+func pingSync() {
+	for _, address := range database.Websites {
+		pingAddress(address)
+	}
+}
 
 func fetchHttpAddress(collection *mongo.Collection, address string) {
 	// Measure time to perform a HTTP GET request on the address
@@ -80,28 +82,28 @@ func fetchHttpAddress(collection *mongo.Collection, address string) {
 	database.UpdateDatabase(collection, address, ping)
 }
 
-// func httpAsyncContinuous() {
-// 	for _, address := range database.Websites {
-// 		go fetchHttpAddress(address)
-// 	}
-// 	for range time.Tick(time.Minute * 60) {
-// 		for _, address := range database.Websites {
-// 			go fetchHttpAddress(address)
-// 		}
-// 	}
-// }
+func httpAsyncContinuous() {
+	for _, address := range database.Websites {
+		go fetchHttpAddress(address)
+	}
+	for range time.Tick(time.Minute * 60) {
+		for _, address := range database.Websites {
+			go fetchHttpAddress(address)
+		}
+	}
+}
 
-// func httpAsync() {
-// 	var wg sync.WaitGroup
-// 	for _, address := range database.Websites {
-// 		wg.Add(1)
-// 		go func(address string) {
-// 			defer wg.Done()
-// 			fetchHttpAddress(address)
-// 		}(address)
-// 	}
-// 	wg.Wait()
-// }
+func httpAsync() {
+	var wg sync.WaitGroup
+	for _, address := range database.Websites {
+		wg.Add(1)
+		go func(address string) {
+			defer wg.Done()
+			fetchHttpAddress(address)
+		}(address)
+	}
+	wg.Wait()
+}
 
 func httpSync() {
 	collection := database.ConnectToDatabase()
